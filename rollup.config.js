6ba8dd1b9ecc690d05b17d5f4da547cdf1abc8b3
zhaoyuqiqi <1940988349@qq.com>
2023-07-14 00:44:11 +0800

    feat: 压缩打包后的包体积

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 export default defineConfig([
   {
     output: {
@@ -8,8 +9,9 @@ export default defineConfig([
     },
     input: "./src/index.ts",
     plugins: [
+      terser(),
       typescript({
-        compilerOptions: { lib: ["es5", "es6", "dom"], target: "es5" },
+        compilerOptions: { lib: ["es5", "es6", "dom"], target: "es6" },
         module: "esnext",
       }),
     ],
